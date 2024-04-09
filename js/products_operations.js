251937c31c2ab2e2f3c61d9785c7c183ef435cb8
AndreaGiuliotti <andrea.giuliotti@iisviolamarchesini.edu.it>
2024-04-09 17:11:49 +0200

    fix postProduct()

--- js/products_operations.js
+++ js/products_operations.js
@@ -1,7 +1,7 @@
 function fetchData() {
     var tableBody = document.getElementById('productTable');
     tableBody.innerText = "";
-    fetch('http://localhost:8080/products')
+    fetch('http://localhost:10000/products')
         .then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok');
@@ -34,7 +34,7 @@ window.onload = function () {
 }
 
 function find(id) {
-    return fetch(`http://localhost:8080/products/${id}`)
+    return fetch(`http://localhost:10000/products/${id}`)
         .then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok');
@@ -48,6 +48,76 @@ function find(id) {
 
 var formModale;
 
+function formPost() {
+    const modalContent = document.getElementById('modalBody');
+    modalContent.innerHTML = `
+                <h2>Crea Prodotto</h2>
+                <p>Nome : <input type="text" id="nome" placeholder="Nome" required></p>
+                <p>Marca : <input type="text" id="marca" placeholder="Marca" required></p>
+                <p>Prezzo : <input type="number" id="prezzo" placeholder="Prezzo" step="0.01" required></p>
+            `;
+    formModale = new bootstrap.Modal(document.getElementById('modalProduct'));
+    var postButton = document.getElementById('primario');
+    postButton.hidden = false;
+    postButton.setAttribute('onclick', `postProduct()`);
+    document.getElementById('close').addEventListener('click', function () {
+        // Chiudi il modal
+        formModale.hide();
+    });
+    formModale.show();
+}
+
+function getJsonApi(idP = null, nomeP, marcaP, prezzoP) {
+    return {
+        data: [
+            {
+                id: idP,
+                type: "products",
+                attributes: {
+                    nome: nomeP,
+                    marca: marcaP,
+                    prezzo: prezzoP
+                }
+            }
+        ]
+    };
+}
+
+function postProduct() {
+    var data = getJsonApi(null,document.getElementById("nome").value, document.getElementById("marca").value, document.getElementById("prezzo").value)
+    fetch(`http://localhost:10000/products`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Errore durante la richiesta POST');
+            }
+            return response.json();
+        })
+        .then(data => {
+            var tableBody = document.getElementById('productTable');
+            var row = data.data;
+            var tr = document.createElement('tr');
+            tr.id = row.id;
+            tr.innerHTML = '<td>' + row.id + '</td>' +
+                '<td>' + row.attributes.nome + '</td>' +
+                '<td>' + row.attributes.marca + '</td>' +
+                '<td>' + row.attributes.prezzo + '</td>' +
+                '<td><button onclick="showProduct(' + row.id + ')" data-toggle="modal" data-target="productModal">Show</button></td>' +
+                '<td><button onclick="editProduct(' + row.id + ')"  data-toggle="modal" data-target="productModal">Edit</button></td>' +
+                '<td><button onclick="deleteProduct(' + row.id + ')"  data-toggle="modal" data-target="productModal">Delete</button></td>';
+            tableBody.appendChild(tr);
+            formModale.hide();
+        })
+        .catch(error => {
+            console.error('Errore durante la richiesta POST:', error);
+        });
+}
+
 function showProduct(id) {
     find(id)
         .then(product => {
@@ -60,13 +130,12 @@ function showProduct(id) {
             `;
             formModale = new bootstrap.Modal(document.getElementById('modalProduct'));
             document.getElementById('primario').hidden = true;
-                this.hidden = true;
-            document.getElementById('close').addEventListener('click', function() {
+            this.hidden = true;
+            document.getElementById('close').addEventListener('click', function () {
                 // Chiudi il modal
                 formModale.hide();
             });
             formModale.show();
-
         })
         .catch(error => {
             console.error('Error fetching product:', error);
@@ -111,13 +180,11 @@ function confirmDelete(id) {
         .then(response => {
             if (response.ok) {
                 window.alert('Prodotto eliminato');
+                formModale.hide();
+                var deleted = document.getElementById(id);
+                deleted.parentNode.removeChild(deleted);
             } else {
                 window.alert("Errore durante l'eliminazione del prodotto");
             }
-        })
-        .finally(() => {
-            formModale.hide(); // Nasconde la modale dopo l'eliminazione
-            var deleted = document.getElementById(id);
-            deleted.parentNode.removeChild(deleted);
         });
 }
